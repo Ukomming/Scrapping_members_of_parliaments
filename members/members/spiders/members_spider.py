c212c3c776e8b2d69782651dd583e8965834a1d1
Ukomming <francisopata09@gmail.com>
2022-10-27 20:08:31 +0000

    saving the output into json and csv format 	modified:   members/members/Members.csv 	new file:   members/members/__init__.py 	new file:   members/members/__pycache__/__init__.cpython-39.pyc 	new file:   members/members/__pycache__/items.cpython-39.pyc 	new file:   members/members/__pycache__/settings.cpython-39.pyc 	new file:   members/members/middlewares.py 	new file:   members/members/pipelines.py 	new file:   members/members/settings.py 	new file:   members/members/spiders/__init__.py 	new file:   members/members/spiders/__pycache__/__init__.cpython-39.pyc 	new file:   members/members/spiders/__pycache__/members_spider.cpython-39.pyc 	modified:   members/members/spiders/members_spider.py 	new file:   members/scrapy.cfg

--- members/members/spiders/members_spider.py
+++ members/members/spiders/members_spider.py
@@ -4,50 +4,27 @@ from ..items import MembersItem
 
 class MembersSpider(scrapy.Spider):
     name = 'Members'
-    start_urls = [
+    start_urls =[
         'https://www.parliament.gh/mps?az&P=0',
-        'https://www.parliament.gh/mps?az&P=a',
-        'https://www.parliament.gh/mps?az&P=14',
-        'https://www.parliament.gh/mps?az&P=1e',
-        'https://www.parliament.gh/mps?az&P=28',
-        'https://www.parliament.gh/mps?az&P=32',
-        'https://www.parliament.gh/mps?az&P=3c',
-        'https://www.parliament.gh/mps?az&P=46',
-        'https://www.parliament.gh/mps?az&P=50',
-        'https://www.parliament.gh/mps?az&P=5a',
-        'https://www.parliament.gh/mps?az&P=64',
-        'https://www.parliament.gh/mps?az&P=6e',
-        'https://www.parliament.gh/mps?az&P=78',
-        'https://www.parliament.gh/mps?az&P=82',
-        'https://www.parliament.gh/mps?az&P=8c',
-        'https://www.parliament.gh/mps?az&P=96',
-        'https://www.parliament.gh/mps?az&P=a0',
-        'https://www.parliament.gh/mps?az&P=aa',
-        'https://www.parliament.gh/mps?az&P=b4',
-        'https://www.parliament.gh/mps?az&P=be',
-        'https://www.parliament.gh/mps?az&P=c8',
-        'https://www.parliament.gh/mps?az&P=d2',
-        'https://www.parliament.gh/mps?az&P=dc',
-        'https://www.parliament.gh/mps?az&P=e6',
-        'https://www.parliament.gh/mps?az&P=f0',
-        'https://www.parliament.gh/mps?az&P=fa',
-        'https://www.parliament.gh/mps?az&P=104',
-        'https://www.parliament.gh/mps?az&P=10e'
     ]
+
 
     def parse(self, response):
         items = MembersItem()
         names = len(response.xpath("//center/b[@class='padd']/text()").getall())
         for num in range(0, names):
-            yield {
-                'name': response.xpath("//center/b[@class='padd']/text()").getall()[num],
-                'party': response.xpath("//center/p/text()").getall()[num],
-                'constituency': response.xpath("//center/b[2]/text()").getall()[num],
-                'region': response.xpath("//p/following-sibling::span/text()").getall()[num]
+            name = response.xpath("//center/b[@class='padd']/text()").getall()[num],
+            party = response.xpath("//center/p/text()").getall()[num],
+            constituency = response.xpath("//center/b[2]/text()").getall()[num],
+            region = response.xpath("//p/following-sibling::span/text()").getall()[num]
+
+            items['name'] = name
+            items['party'] = party
+            items['constituency'] = constituency
+            items['region'] = region
+            yield items
+            for a in response.xpath("//a[@class='square goldenrod']/@href").getall():
+                yield response.follow(a, callback=self.parse)
 
 
-            }
-# response.xpath("//center/b[@class='padd']/text()").getall()   name
-# response.xpath("//center/p/text()").getall()    party
-# response.xpath("//center/b[2]/text()").getall()  constituency
-# response.xpath("//center/span/text()").getall()  region
+
